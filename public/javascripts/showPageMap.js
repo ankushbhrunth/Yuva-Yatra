04f198ef240adca68fab739a8db132e1affb7307
ankushbhrunth <79744843+ankushbhrunth@users.noreply.github.com>
2023-08-05 16:35:38 +0530

    Add files via upload

---
+++ public/javascripts/showPageMap.js
@@ -0,0 +1,19 @@
+mapboxgl.accessToken = mapToken;
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    center: campground.geometry.coordinates, // starting position [lng, lat]
+    zoom: 4 // starting zoom
+    });
+    map.addControl(new mapboxgl.NavigationControl());
+
+
+new mapboxgl.Marker()
+    .setLngLat(campground.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({offset:25})
+        .setHTML(
+            `<h5>${campground.title}</h5><p>${campground.location}</p>`
+        )
+    )
+    .addTo(map)
